f3a0fff24f347a8cd95166c0ce845f2254979c1b
AleksandraVysitska <Gamberetto.17>
2022-04-27 22:59:29 +0200

    exclude dataSource

--- src/main/java/com/design/patterns/PatternsApplication.java
+++ src/main/java/com/design/patterns/PatternsApplication.java
@@ -2,8 +2,9 @@ package com.design.patterns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class PatternsApplication {
 
 	public static void main(String[] args) {
